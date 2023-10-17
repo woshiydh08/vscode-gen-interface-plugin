f36101ece8057474c9499b379f3fa9cb47c87f81
尤东华 <wolong_you@ikscrm.com>
2023-10-17 17:18:47 +0800

    feat: 新增合并功能

--- src/generateTypescriptFile.ts
+++ src/generateTypescriptFile.ts
@@ -1,62 +1,30 @@
 import * as fs from 'fs';
-
 import * as path from 'path';
-/**
- * 将字符串的首字母大写
- * @param str 输入的字符串
- * @returns 首字母大写后的字符串
- */
-function capitalizeFirstLetter(str: string): string {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
 
-/**
- * 生成接口定义
- * @param comment 注释名
- * @param functionName 函数名
- * @returns 接口定义的字符串
- */
-function generateInterface(comment: string, functionName: string): string {
-  /** 定义请求参数接口 */
-  const requestInterface = `/** ${comment}参数 */
-export interface ${capitalizeFirstLetter(functionName)}Request {
-
-}
-
-`;
-  /** 定义响应结果接口 */
-  const responseInterface = `/** ${comment}响应 */
-export interface ${capitalizeFirstLetter(functionName)}Response {
-
-}
-
-`;
-
-  return requestInterface + responseInterface;
-}
-
-/**
- * 解析给定的文件路径并获取文件内容
- * @param file JavaScript文件的路径
- * @returns
- */
-function readFileContent(file: string): string {
-  const filePath = path.resolve(file);
-  return fs.readFileSync(filePath, 'utf-8');
-}
+import {
+  capitalizeFirstLetter,
+  generateRequestInterface,
+  generateResponseInterface,
+  mergeObjects,
+  parseInterface,
+  readFileContent,
+} from './utils';
 
 /**
  * 生成TypeScript文件
  * @param file JavaScript文件的路径
  * @param isOverWrite 是否覆盖原有文件
  */
-function generateTypescriptFile(sourceFilePath: string, force: boolean  = false): void {
+function generateTypescriptFile(
+  sourceFilePath: string,
+  force: boolean = false
+): void {
   const content = readFileContent(sourceFilePath);
 
   // 使用正则表达式匹配导出函数的名称
   const regex = /\/\*\*\s*(.*?)\s*\*\/\s*export const (\w+)\s*=/g;
   let match;
-  let interfaces = '';
+  const interfaces: { [key: string]: string } = {};
 
   while ((match = regex.exec(content)) !== null) {
     /** 匹配到的注释内容 */
@@ -64,7 +32,10 @@ function generateTypescriptFile(sourceFilePath: string, force: boolean  = false)
     /** 匹配到的函数名称 */
     const functionName = match[2];
     // 调用函数生成接口定义并拼接到接口字符串中
-    interfaces += generateInterface(comment, functionName);
+    interfaces[`${capitalizeFirstLetter(functionName)}Request`] =
+      generateRequestInterface(comment, functionName);
+    interfaces[`${capitalizeFirstLetter(functionName)}Response`] =
+      generateResponseInterface(comment, functionName);
   }
 
   const outputFileName = `${path.parse(sourceFilePath).name}.type.ts`;
@@ -75,13 +46,40 @@ function generateTypescriptFile(sourceFilePath: string, force: boolean  = false)
     throw new Error('文件已存在，无法写入');
   }
 
-  fs.writeFileSync(outputFile, interfaces, 'utf-8');
+  // 检查是否存在需要合并的接口文件
+  const interfaceFile = `${path.parse(sourceFilePath).name}.type.ts`;
+  const interfaceFilePath = path.join(
+    path.dirname(sourceFilePath),
+    interfaceFile
+  );
+  let parsedInterfaces: { [key: string]: string } | undefined = undefined;
+  if (fs.existsSync(interfaceFilePath)) {
+    const existingInterfaces = readFileContent(interfaceFilePath);
+    parsedInterfaces = parseInterface(existingInterfaces);
+    console.log(
+      '🌊 ~ file: generateTypescriptFile.ts:58 ~ interfaces:',
+      interfaces
+    );
+
+    console.log(
+      '🌊 ~ file: generateTypescriptFile.ts:48 ~ existingInterfaces:',
+      parsedInterfaces
+    );
+  }
+
+  // 将接口对象转换为字符串
+  const interfacesString = Object.values(
+    parsedInterfaces ? mergeObjects(interfaces, parsedInterfaces) : interfaces
+  ).join('\n\n');
+  console.log("🌊 ~ file: generateTypescriptFile.ts:74 ~ interfacesString:", interfacesString)
+
+  fs.writeFileSync(outputFile, interfacesString, 'utf-8');
 }
 
 /**
  * 根据路径文件的函数, 生成接口文件
  */
-export function build(filePath: string, force: boolean  = false): void {
+export function build(filePath: string, force: boolean = false): void {
   // 获取命令行参数并生成TypeScript文件
   generateTypescriptFile(filePath, force);
 }
@@ -90,5 +88,8 @@ export function build(filePath: string, force: boolean  = false): void {
  * TODO
  * 先读取xxx.type.ts文件
  * 如果有内容, 先把内容拿出来
- * 
+ *
+ * 1. 生成文件
+ * 2. 对比新生成的内容和已有文件的内容
+ * 3. 如果已有文件内容中含有与
  */
---
+++ src/utils/index.ts
@@ -0,0 +1,124 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+/**
+ * 将字符串的首字母大写
+ * @param str 输入的字符串
+ * @returns 首字母大写后的字符串
+ */
+export function capitalizeFirstLetter(str: string): string {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export function generateRequestInterface(comment: string, functionName: string) {
+  return `/** ${comment}参数 */
+export interface ${capitalizeFirstLetter(functionName)}Request {
+
+}`;
+}
+
+export function generateResponseInterface(comment: string, functionName: string) {
+  return `/** ${comment}响应 */
+export interface ${capitalizeFirstLetter(functionName)}Response {
+
+}`;
+}
+
+/**
+ * 生成接口定义
+ * @param comment 注释名
+ * @param functionName 函数名
+ * @returns 接口定义的字符串
+ */
+export function generateInterface(
+  comment: string,
+  functionName: string
+): string {
+  /** 定义请求参数接口 */
+  const requestInterface = generateRequestInterface(comment, functionName);
+  /** 定义响应结果接口 */
+  const responseInterface = generateResponseInterface(comment, functionName);
+
+  return requestInterface + responseInterface;
+}
+
+/**
+ * 解析给定的文件路径并获取文件内容
+ * @param file JavaScript文件的路径
+ * @returns
+ */
+export function readFileContent(file: string): string {
+  const filePath = path.resolve(file);
+  return fs.readFileSync(filePath, 'utf-8');
+}
+
+/**
+ * 辅助函数：找到接口的结尾位置
+ * @param code 源代码字符串
+ * @param startIndex 接口起始位置的索引
+ * @returns 接口的结尾位置的索引，如果找不到则返回 -1
+ */
+export function findInterfaceEndIndex(
+  code: string,
+  startIndex: number
+): number {
+  let curlyBracketsCount = 1; // 记录大括号的数量，初始为1，表示接口的起始大括号
+  let index = startIndex;
+
+  while (curlyBracketsCount > 0 && index < code.length) {
+    if (code[index] === '{') {
+      curlyBracketsCount++;
+    } else if (code[index] === '}') {
+      curlyBracketsCount--;
+    }
+
+    index++;
+  }
+
+  return curlyBracketsCount === 0 ? index : -1;
+}
+
+export function parseInterface(sourceCode: string): { [key: string]: string } {
+  const interfaceRegex = /\/\*\*(.*?)\*\/\s*export\s*interface\s*(\w+)\s*{/gs;
+  const interfaces: Record<string, string> = {};
+
+  let match;
+  while ((match = interfaceRegex.exec(sourceCode))) {
+    const comment = match[1].trim();
+    const interfaceName = match[2];
+
+    interfaces[interfaceName] = `/** ${comment} */
+export interface ${interfaceName} {`;
+
+    // 寻找接口的结尾位置
+    const endIndex = findInterfaceEndIndex(
+      sourceCode,
+      match.index + match[0].length
+    );
+    if (endIndex !== -1) {
+      interfaces[interfaceName] += sourceCode.substring(
+        match.index + match[0].length,
+        endIndex
+      );
+    }
+  }
+
+  return interfaces;
+}
+
+
+export function mergeObjects(a: {[key: string]: string}, b: {[key: string]: string}): {[key: string]: string} {
+  const result = {...b};
+
+  for (const [key, value] of Object.entries(a)) {
+    if (b.hasOwnProperty(key)) {
+      // 如果B中有重复键，则使用B中的键值
+      result[key] = b[key];
+    } else {
+      // 如果B中没有重复键，则使用A中的键值
+      result[key] = value;
+    }
+  }
+
+  return result;
+}
